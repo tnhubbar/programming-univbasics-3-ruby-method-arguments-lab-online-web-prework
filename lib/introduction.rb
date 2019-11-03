2aeb974d5e15c9540063d0141de1945fd5c4e65f
Taryn Johnson <taryn.johnson919@gmail.com>
2019-11-03 02:47:05 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,19 +2,19 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-introduction(Taryn)
+introduction("Taryn")
 
 def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-introduction_with_language(Taryn, Ruby)
+introduction_with_language("Taryn", "Ruby")
 
 def introduction_with_language_optional(name, language = 'Ruby')
  puts "Hi, my name is #{name} and I am learning to program in #{language}."
  end
  
-introduction_with_language_optional(Taryn)
+introduction_with_language_optional("Taryn")
 
 
 
